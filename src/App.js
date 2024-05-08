ec9610d6fc994051770ac606a2b33bda851e562b
unknown <misterperfect@gmail.com>
2024-05-08 10:02:15 +0530

    Quiz added

--- src/App.js
+++ src/App.js
@@ -18,8 +18,6 @@ function App() {
     setQuestions(data.results);
   }
 
-  //https://opentdb.com/api.php?amount=20&category=${9 to 32}&difficulty=${easy or medium or hard}&type=multiple
-
   return (
     <BrowserRouter>
       <div className="App">
